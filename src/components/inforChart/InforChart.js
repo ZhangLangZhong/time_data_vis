8585b0116bfb99c6079092ddaa3b8433a60e50ca
“ZhangLangZhong” <as466931001@qq.com>
2022-09-03 14:17:40 +0800

    change

--- src/components/inforChart/InforChart.js
+++ src/components/inforChart/InforChart.js
@@ -1,37 +1,18 @@
 import React, { useEffect } from 'react'
 import './InforChart.css'
-import { sendAction } from '../../redux/action';
+// import { sendAction } from '../../redux/action';
 
-import store from '../../redux/store';
-import { Provider } from 'react-redux'
-import ComA from './comA';
-import ComB from './comB';
+// import store from '../../redux/store';
+// import { Provider } from 'react-redux'
+
 
 export default function InforChart() {
 
-  // const handleClick = () => {
-  //   const action = sendAction()
-  //   // 发送action
-  //   store.dispatch(action)
-  // }
-
-  // useEffect(()=>{
-  //   store.subscribe(()=>{
-  //     console.log('subscribe',store.getState())
-  //   })
-  // },[])
-
-
   return (
-    <Provider store={store}>
-      <div className='inforchart'>
-        {/* inforchart */}
-        <ComA></ComA>
-        <ComB></ComB>
-      </div >
-    </Provider>
-    
 
+      <div className='inforchart'>
+        
+      </div >
 
   )
 }
